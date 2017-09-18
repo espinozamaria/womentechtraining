2bc61e7b184d1cffcf0f176afdd5c97088565dda
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-09-18 16:19:38 -0700

    Added map to contact page and modified navbar styles

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ $(function(){
     keyboard: false
   });
 });
+
+function myMap() {
+  var uluru = {lat: 36.202447, lng: -120.103318};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
